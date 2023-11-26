1d746f148fb38898d707cb78830116949a292b70
kyufuru <kyufuru@163.com>
2023-11-27 06:25:25 +0800

    物理系统: 重力

--- Animitta/scene/player/player_test.gd
+++ Animitta/scene/player/player_test.gd
@@ -1,38 +1,37 @@
 extends CharacterBody2D
 
-var v: int = 500
-var d: Vector2 = Vector2.ZERO
+@export var speed: int
+@export var mass: int
 
-var can_m_act: bool = true
-var can_s_act: bool = true
-
-signal m_act
-signal s_act
-
-func _physics_process(delta):
-#	position = position.round()
+func _ready():
 	pass
+
+
+func _physics_process(_delta):
+	press()
+	move()
+
+## 处理按键
+func press() -> void:
+#	# 动作
+#	if Input.is_action_pressed("m_act") and can_m_act:
+#		can_m_act = false
+#		$TimerMAct.start()
+#		m_act.emit($Mks/MkMAct.global_position)
+#
+#	if Input.is_action_pressed("s_act") and can_s_act:
+#		can_s_act = false
+#		$TimerSAct.start()
+#		s_act.emit()
+	pass
+
+## 处理移动
+func move() -> void:
+	# 移动
+	velocity = speed * Input.get_vector(
+			"left","right","up","down")
 	
-func _process(_delta):
-	# 方向
-	d = Input.get_vector("left","right","up","down")
-	velocity = v * d
+	# 重力
+	velocity.y += mass * $"../Physics".Gravity
+	
 	move_and_slide()
-	
-	# 动作
-	if Input.is_action_pressed("m_act") and can_m_act:
-		can_m_act = false
-		$TimerMAct.start()
-		m_act.emit($Mks/MkMAct.global_position)
-		
-	if Input.is_action_pressed("s_act") and can_s_act:
-		can_s_act = false
-		$TimerSAct.start()
-		s_act.emit()
-
-func _on_timer_timeout():
-	can_m_act = true
-
-
-func _on_timer_s_act_timeout():
-	can_s_act = true
