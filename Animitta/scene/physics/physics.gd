7dc48e21bdcbecb002a30d964d37a7a148e1036b
kyufuru <kyufuru@163.com>
2023-11-27 20:33:41 +0800

    修复抖动的问题, 实现跳跃功能

--- Animitta/scene/physics/physics.gd
+++ Animitta/scene/physics/physics.gd
@@ -1,12 +1,4 @@
 extends Node2D
 
-enum States {
-	IDLE,
-	JUMP,
-	HIGH_JUMP,
-	DASH,
-	ATTACK,
-}
-
 @export var gravity: int
 
--- Animitta/scene/player/player_test.gd
+++ Animitta/scene/player/player_test.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 
-@export var cur_state: int
+enum States {
+	IDLE,
+	JUMP,
+	HIGH_JUMP,
+	DASH,
+	ATTACK,
+}
+const x_msk := Vector2(1, 0)
 
+@export var cur_state: int
 @export var jump_speed: int
 @export var speed: int
 @export var mass: int
 
-const x_msk := Vector2(1, 0)
+var g: int
 
+func _ready():
+	g = %Physics.gravity
 
 func _physics_process(delta):
 	press()
@@ -17,17 +27,25 @@ func _physics_process(delta):
 ## 处理按键
 func press() -> void:
 	if Input.is_action_just_pressed("jump"):
-		cur_state = %Physics.States.JUMP
+		if cur_state != States.JUMP:
+			cur_state = States.JUMP
+	else:
+		cur_state = States.IDLE
 
 
 func act(_dt: float) -> void:
-	if cur_state == %Physics.States.JUMP:
-		velocity.y -= jump_speed * %Physics.gravity
+	if cur_state == States.JUMP:
+		if is_on_floor():
+			velocity.y -= jump_speed * g
+	if cur_state == States.IDLE:
+		if is_on_floor():
+			velocity.y = 0
 
 
 ## 处理物理移动
 func move(_dt: float) -> void:
-	velocity = x_msk * speed * Input.get_vector(
-		"left","right","up","down") # 移动
-	velocity.y += mass * %Physics.gravity # 重力
+	velocity.x = speed * Input.get_vector(
+		"left","right","up","down").x # 移动
+	if not is_on_floor():
+		velocity.y += mass * g # 重力
 	move_and_slide()
