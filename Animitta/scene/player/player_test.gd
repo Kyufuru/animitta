1da96349a0dc40df674fb85e8546c41826c2644b
kyufuru <kyufuru@163.com>
2023-12-18 13:40:05 +0800

    231218

--- Animitta/scene/player/player_test.gd
+++ Animitta/scene/player/player_test.gd
@@ -6,19 +6,18 @@ var g: int = Global.gravity
 var i: int = 0
 
 func _physics_process(_delta):
-	var s = Input.get_vector("left","right","up","down")
+	var d = Input.get_vector("left","right","up","down")
 	var is_jump = Input.is_action_just_pressed("jump")
 	var is_atk = Input.is_action_just_pressed("m_act")
 	var is_dash = Input.is_action_just_pressed("s_act")
 
-	velocity.x = s.x * v.x
-		
+	velocity.x = d.x * v.x
+
 	if not is_on_floor():
 		velocity.y += g
 	elif is_jump:
 		velocity.y -= v.y
 	else:
-		s.y = 0
 		velocity.y = 0
 
 	i += 1
